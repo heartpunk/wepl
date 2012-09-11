071f1aa219179912eb8179316a52c28588c9c8f3
Ezekiel Smithburg <esmithburg@marchex.com>
2012-09-10 21:49:59 -0700

    added a bit of a nicer interface.

--- wepl.js
+++ wepl.js
@@ -1,25 +1,37 @@
 var express = require('express');
+var util = require('util');
+
 var app = express();
+
+app.engine('jade', require('jade').__express);
 
 var port = process.env.PORT || 3000;
 var global = this;
 
 app.get('/', function(req, res){
-  if (req.query.methodVal) {
-    app.get(('/' + req.query.methodName), function(req1,res1) {
-      res1.send(req.query.methodVal);
-    });
-    res.send(req.query.methodName + ' was defined.  it returns: ' + req.query.methodVal);
-    console.log('responded from methodVal branch');
-  }
-  else if (req.query.methodBody) {
-    with(global) {eval(req.query.methodBody)};
-    res.send('<pre>' + req.query.methodBody + " was evaled.");
-    console.log('responded from methodBody branch');
+  if (req.query.methodBody) {
+    var result;
+    with(global) {result = util.inspect(eval(req.query.methodBody), true, 3)};
+    res.render(
+      'index.jade',
+      {
+        code: req.query.methodBody,
+        result: result
+      }
+    );
+    console.log('got some code.');
   }
   else {
-    res.send('welcome to the wepl.');
-    console.log('responded from default branch.');
+    demo_code = "Math.exp(10);";
+    demo_result = util.inspect(eval(demo_code), true, 3);
+    res.render(
+      'index.jade',
+      {
+        code: demo_code,
+        result: demo_result
+      }
+    );
+    console.log('main page.');
   }
 });
 
